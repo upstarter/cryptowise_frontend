d7d8039982ea905061bd2775f0d440ec667e6232
Eric Steen <ericsteen1@gmail.com>
2019-10-12 16:16:37 -0700

    n

--- bootstart.sh
+++ bootstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nginx-debug -g "daemon off";
+nginx-debug -g "daemon off;"
 /usr/local/bin/envoy -c /etc/frontend-envoy.yaml --service-cluster frontend --component-log-level "upstream:debug"
