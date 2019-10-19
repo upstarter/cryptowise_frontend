cd368967548a916ef5b49d21d65a3fef3e70ce57
Eric Steen <ericsteen1@gmail.com>
2019-10-19 11:18:24 -0700

    n

--- src/js/react_app/src/config/axios.js
+++ src/js/react_app/src/config/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import AuthService from 'Services/auth/AuthService';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 axios.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.common['Authorization'] = `Bearer ${AuthService.getToken()}`;
+axios.defaults.headers.common['Authorization'] = `Bearer ${cookies.get('_cw_acc')}`;
 axios.defaults.headers.common['X-CSRF-TOKEN'] = localStorage.getItem('csrf');
 axios.defaults.withCredentials = true;
 // Also add/ configure interceptors && all the other cool stuff
