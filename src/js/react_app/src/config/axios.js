7ea528bdb665d88f51537550964462186e8dea3f
Eric Steen <eric.steen@important.com>
2021-04-26 12:41:46 -0500

    more

--- src/js/react_app/src/config/axios.js
+++ src/js/react_app/src/config/axios.js
@@ -9,7 +9,7 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${cookies.get('_cw_acc'
 axios.defaults.headers.common['X-CSRF-TOKEN'] = cookies.get('_cw_csrf');
 axios.defaults.headers.common['Origin'] = 'https://www.cryptowise.ai'
 axios.defaults.headers.common['credentials'] = 'same-origin'
-axios.defaults.withCredentials = false;
+axios.defaults.withCredentials = true;
 // Also add/ configure interceptors && all the other cool stuff
 //
 // instance.interceptors.request...
