95def9675f8a44b8856b405c4ca1d2de50df5348
Eric Steen <eric.steen@important.com>
2021-04-26 17:58:45 -0500

    more

--- bootstart.sh
+++ bootstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-service nginx start
+service nginx start -c /etc/nginx/sites-enabled/default
 /usr/local/bin/envoy -c /etc/frontend-envoy.yaml --service-cluster frontend --component-log-level  upstream:debug,connection:trace
