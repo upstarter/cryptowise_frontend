0311d83e85a61df0b112f701a36ddf7a2423158a
Eric Steen <eric.steen@important.com>
2021-04-26 12:28:54 -0500

    more

--- src/js/react_app/src/config/axios.js
+++ src/js/react_app/src/config/axios.js
@@ -7,16 +7,15 @@ const cookies = new Cookies();
 axios.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.common['Authorization'] = `Bearer ${cookies.get('_cw_acc')}`;
 axios.defaults.headers.common['X-CSRF-TOKEN'] = cookies.get('_cw_csrf');
-// axios.defaults.headers.common['Origin'] = 'https://www.cryptowise.ai'
-// axios.defaults.headers.common['Allow-Control-Request-Headers'] = 'Content-Type, Accept'
-axios.defaults.withCredentials = true;
+axios.defaults.headers.common['Origin'] = 'https://www.cryptowise.ai'
+axios.defaults.headers.common['credentials'] = 'same-origin'
+axios.defaults.withCredentials = false;
 // Also add/ configure interceptors && all the other cool stuff
 //
 // instance.interceptors.request...
 
 const axiosInstance = axios.create({
-  baseURL: "//api.cryptowise.ai",
-  withCredentials: true
+  baseURL: "//api.cryptowise.ai"
 });
 
 export default axiosInstance;
